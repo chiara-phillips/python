a53ebecf8c740529888291967d96c43d7cea432b
Chiara Phillips <chiara.m.phillips@gmail.com>
2022-04-12 22:08:19 -0700

    Update EquationPlot.py

--- EquationPlot.py
+++ EquationPlot.py
@@ -1,6 +1,7 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
+# Storing each function in a different variable
 r17 = 2000000000/(pow(x,2.7745))
 s17 = 19400000000/(pow(x,3.06))
 r18 = 300000000/(pow(x,2.41))
@@ -8,7 +9,7 @@ s18 = 351000000/(pow(x,2.41))
 r19 = 71731/(pow(x,1.347))
 s19 = 116013/(pow(x,1.41))
 
-# Create the plot
+# Adding each function to the plot with a label
 plt.plot(x, r17, label='R2017')
 plt.plot(x, s17, label=' S2017')
 plt.plot(x, r18, label='R2018')
@@ -16,7 +17,7 @@ plt.plot(x, s18, label='S2018')
 plt.plot(x, r19, label='R2019')
 plt.plot(x, s19, label='S2019')
 
-# Show the plot
+# Adding titles, legend, gridlines
 plt.title('Band 12/SOC Regression Functions')
 plt.xlabel('Band 12 Value')
 plt.ylabel('SOC Percent')
@@ -24,3 +25,6 @@ plt.legend()
 #plt.ylim([0, 6])
 #plt.xlim([1200, 2500])
 plt.grid(alpha=.4,linestyle='--')
+
+# Showing final plot
+plt.show()
