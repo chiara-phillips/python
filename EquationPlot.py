c4d32824c7cc7bb8aece18f2e59554dd4b5bb507
Chiara Phillips <chiara.m.phillips@gmail.com>
2022-04-12 21:56:46 -0700

    Create EquationPlot.py

---
+++ EquationPlot.py
@@ -0,0 +1,26 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+r17 = 2000000000/(pow(x,2.7745))
+s17 = 19400000000/(pow(x,3.06))
+r18 = 300000000/(pow(x,2.41))
+s18 = 351000000/(pow(x,2.41))
+r19 = 71731/(pow(x,1.347))
+s19 = 116013/(pow(x,1.41))
+
+# Create the plot
+plt.plot(x, r17, label='R2017')
+plt.plot(x, s17, label=' S2017')
+plt.plot(x, r18, label='R2018')
+plt.plot(x, s18, label='S2018')
+plt.plot(x, r19, label='R2019')
+plt.plot(x, s19, label='S2019')
+
+# Show the plot
+plt.title('Band 12/SOC Regression Functions')
+plt.xlabel('Band 12 Value')
+plt.ylabel('SOC Percent')
+plt.legend()
+#plt.ylim([0, 6])
+#plt.xlim([1200, 2500])
+plt.grid(alpha=.4,linestyle='--')
